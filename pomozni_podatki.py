e2c2e0528debbe1dc07e4bbf0850a4420f18eac9
Benjamin Rajer <benjamin.rajer@gmail.com>
2026-08-11 22:18:33 +0200

    Dodaj .gitignore, shrani prvi HTML

--- pomozni_podatki.py
+++ pomozni_podatki.py
@@ -1,5 +1,29 @@
+# encoding za spletno stran je ISO-8859-1
+
+# glave za HTTP zahtevo - pomagajo, da nam zahteva uspe
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
 }
+
+
+#-------------------------------URL-JI---------------------------------
+# podatki o sezoni
+def SEZONA_URL(leto):
+    return f"https://www.basketball-reference.com/leagues/NBA_{leto}_standings.html"
+
+
+# podatki o ekipi v sezoni, vsaka ekipa je podana s tričkrovno kodo
+def EKIPA_URL(koda, leto):
+    return f"https://www.basketball-reference.com/teams/{koda}/{leto}.html"
+
+
+# splošni podatki o igralcu, igralčev id je sestavljen iz delov imena in priimka ter iz številke
+def IGRALEC_URL(id):
+    return f"https://www.basketball-reference.com/players/{id[0]}/{id}.html"
+
+
+# podatki o igralcu v sezoni
+def IGRALEC_SEZONA_URL(id, leto):
+    return f"https://www.basketball-reference.com/players/{id[0]}/{id}/gamelog/{leto}/"
--- pridobivanje_podatkov.py
+++ pridobivanje_podatkov.py
@@ -1,4 +1,29 @@
 import requests
 import time
+
 from bs4 import BeautifulSoup
+import re
+
 import pomozni_podatki as POM
+
+
+# tričrkovne kode ekip pobere iz razpredelnice o konferencah, to uporabi tudi za id
+def pridobi_kodo_ekip(soup):
+    ...
+
+
+
+def podatki_sezona(leto):
+
+    stran = requests.get(POM.SEZONA_URL(leto), headers=POM.HEADERS)
+
+    with open(f"UVP_projektna_naloga/spletne_strani/sezona_{leto}.html", "w", encoding="ISO-8859-1") as dat:
+        dat.write(stran.text)
+
+    soup = BeautifulSoup(stran.text, "html.parser")
+
+    time.sleep(5)
+
+
+
+podatki_sezona(2026)
