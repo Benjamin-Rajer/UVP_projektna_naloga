1fb4d7d1901ef3e592763850b7212bfb22d34093
Benjamin Rajer <benjamin.rajer@gmail.com>
2026-08-17 01:09:00 +0200

    Pridobi podatke iz konferenc

--- pomozni_podatki.py
+++ pomozni_podatki.py
@@ -1,6 +1,6 @@
 # encoding za spletno stran je ISO-8859-1
 
-# glave za HTTP zahtevo - pomagajo, da nam zahteva uspe
+# glave za HTTP zahtevo
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -13,6 +13,13 @@ HEADERS = {
 def SEZONA_URL(leto):
     return f"https://www.basketball-reference.com/leagues/NBA_{leto}_standings.html"
 
+# končnica sezone, 
+def KONCNICA_SEZONE_URL(leto):
+    return f"https://www.basketball-reference.com/playoffs/NBA_{leto}.html"
+
+# podatki o vseh tekmah, razdeljenih po mesecih - oktober do junij
+def TEKME_URL(leto, mesec):
+    return f"https://www.basketball-reference.com/leagues/NBA_{leto}_games-{mesec}.html"
 
 # podatki o ekipi v sezoni, vsaka ekipa je podana s tričkrovno kodo
 def EKIPA_URL(koda, leto):
@@ -27,3 +34,22 @@ def IGRALEC_URL(id):
 # podatki o igralcu v sezoni
 def IGRALEC_SEZONA_URL(id, leto):
     return f"https://www.basketball-reference.com/players/{id[0]}/{id}/gamelog/{leto}/"
+
+
+#------------------------------PREVAJALNIK-------------------------------------
+
+PREVAJALNIK_DIVIZIJE = {
+    # trenutne
+    "Atlantic Division": "Atlantska",
+    "Central Division": "Centralna",
+    "Southeast Division": "Jugovzhodna",
+    "Northwest Division": "Severozahodna",
+    "Pacific Division": "Pacifiška",
+    "Southwest Division": "Jugozahodna",
+
+    # zgodovinske
+    "Eastern Division": "Vzhodna",
+    "Western Division": "Zahodna",
+    "Midwest Division": "Srednjezahodna",
+}
+
--- pridobivanje_podatkov.py
+++ pridobivanje_podatkov.py
@@ -1,29 +1,127 @@
 import requests
 import time
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import re
 
 import pomozni_podatki as POM
 
 
-# tričrkovne kode ekip pobere iz razpredelnice o konferencah, to uporabi tudi za id
+# tričrkovne kode ekip pobere iz razširjene tabele (v HTML datoteki je zakomentirana), to uporabi tudi za id
 def pridobi_kodo_ekip(soup):
-    ...
+    slovar = {}
+
+    tabela = soup.find(
+        string = lambda text: isinstance(text, Comment) and "div_expanded_standings" in text
+            )
+
+    tabela_soup = BeautifulSoup(tabela, "html.parser")
+    ekipe = tabela_soup.find_all("a")
+
+    for ekipa in ekipe:
+        kratica = re.search(r"/teams/([A-Z]+)/\d+.html", ekipa["href"]).group(1)
+        ime = ekipa.text
+        slovar.update({kratica: ime})
+
+    return slovar
 
 
 
-def podatki_sezona(leto):
+# podatki o sezoni, vrne BeautifulSoup
+def pridobi_podatke_sezona(leto):
 
     stran = requests.get(POM.SEZONA_URL(leto), headers=POM.HEADERS)
 
-    with open(f"UVP_projektna_naloga/spletne_strani/sezona_{leto}.html", "w", encoding="ISO-8859-1") as dat:
-        dat.write(stran.text)
+    # to je pomoje boljše odstraniti potem, niti ni treba shraniti spletne strani
+    #with open(f"UVP_projektna_naloga/spletne_strani/sezona_{leto}.html", "w", encoding="ISO-8859-1") as dat:
+    #    dat.write(stran.text)
 
     soup = BeautifulSoup(stran.text, "html.parser")
 
     time.sleep(5)
 
+    return soup
 
 
-podatki_sezona(2026)
+def pridobi_podatke_igralec(id):
+    ...
+
+def izlusci_podatke_sezona(soup, leto):
+    seznam_podatkov = []
+
+    # tabeli po konferencah - od leta 1971 naprej - prej je samo brez E in W, na koncu je underscore
+    # lahko to daš v ločeno funkcijo pa daš še default value da je "" ali pa kaj takega
+    for smer in ["E", "W"]:
+        tabela = soup.find("table", id = f"divs_standings_{smer}").find("tbody")
+        vrstice = tabela.find_all("tr")
+
+        for vrstica in vrstice:
+
+            if vrstica["class"] == "thead": # vrstica, v kateri piše ime divizije
+                divizija = POM.PREVAJALNIK_DIVIZIJE[vrstica.find("strong").text]
+                continue
+
+            # id in ime ekipe
+            ekipa = vrstica.find("a")
+            id_ekipe = re.search(r"/teams/([A-Z]+)/\d+.html", ekipa["href"]).group(1)
+            ime_ekipe = ekipa.text
+
+            # konferenca
+            if smer == "E":
+                konferenca = "vzhodna"
+            else:
+                konferenca = "zahodna"
+
+
+            # zmage, porazi, točke
+
+            def podatek_vrstice(atribut):
+                return vrstica.find("td", attrs = {"data-stat": atribut}).text
+                
+            zmage = podatek_vrstice("wins")
+            porazi = podatek_vrstice("losses")
+            povp_tocke = podatek_vrstice("pts_per_g") # povprečne dane točke na igro
+            povp_prejeto = podatek_vrstice("opp_pts_per_g") # povprečne prejete točke na igro
+
+            seznam_podatkov.append({
+                "id": id_ekipe,
+                "ime": ime_ekipe,
+                "leto": leto,
+                "konferenca": konferenca,
+                "divizija": divizija,
+                "zmage": zmage,
+                "porazi": porazi,
+                "povprečne dane točke": povp_tocke,
+                "povprečne prejete točke": povp_prejeto
+            })
+
+    
+    # tabela razširjenih statistik - zakomentirana (poglej če je za vsa leta zakomentirana)
+    tabela_raz = soup.find(
+            string = lambda text: isinstance(text, Comment) and "div_expanded_standings" in text
+                )
+
+    tabela_soup = BeautifulSoup(tabela_raz, "html.parser").find("tbody")
+    vrstice_raz = tabela_soup.find_all("tr")
+
+    for vrstica in vrstice_raz:
+        ...
+        
+
+
+   
+
+
+    return seznam_podatkov
+
+
+
+
+
+
+# začasno
+with open("UVP_projektna_naloga/spletne_strani/sezona_2026.html", "r",  encoding="ISO-8859-1") as dat:
+    soup1 = BeautifulSoup(dat.read(), "html.parser")
+
+print(izlusci_podatke_sezona(soup1))
+
